b738cd66512a026131ce940116eb23b2ef7abd0a
ettom <36895504+ettom@users.noreply.github.com>
2019-10-28 19:59:11 +0200

    Refactor weather_reporter.h for readability

--- core/weather_reporter.cpp
+++ core/weather_reporter.cpp
@@ -1,28 +1,28 @@
 #include "weather_reporter.h"
 
-Forecast get_forecast(std::string city, WeatherGetter& g)
+Forecast get_forecast(std::string city, WeatherGetter& getter)
 {
 	Forecast f;
-	f.current_weather = get_current_weather(city, g);
-	f.forecasts = get_three_day_forecast(city, g);
+	f.current_weather = get_current_weather(city, getter);
+	f.forecasts = get_three_day_forecast(city, getter);
 	return f;
 }
 
-Report get_current_weather(std::string city, WeatherGetter& g)
+Report get_current_weather(std::string city, WeatherGetter& getter)
 {
 	QueryParameters q;
 	q.city = city;
-	std::string response = g.get_weather_data(q);
+	std::string response = getter.get_weather_data(q);
 
 	Report result;
 	return result;
 }
 
-std::vector<Report> get_three_day_forecast(std::string city, WeatherGetter& g)
+std::vector<Report> get_three_day_forecast(std::string city, WeatherGetter& getter)
 {
 	QueryParameters q;
 	q.city = city;
-	std::string response = g.get_weather_data(q);
+	std::string response = getter.get_weather_data(q);
 
 	std::vector<Report> result;
 	return result;
--- core/weather_reporter.h
+++ core/weather_reporter.h
@@ -2,7 +2,7 @@
 #include "core.h"
 #include "weather_getter.h"
 
-Forecast get_forecast(std::string city, WeatherGetter& g);
+Forecast get_forecast(std::string city, WeatherGetter& getter);
 
-Report get_current_weather(std::string city, WeatherGetter& g);
-std::vector<Report> get_three_day_forecast(std::string city, WeatherGetter& g);
+Report get_current_weather(std::string city, WeatherGetter& getter);
+std::vector<Report> get_three_day_forecast(std::string city, WeatherGetter& getter);
