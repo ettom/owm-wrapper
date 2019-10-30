21efadd28bfdd55fe5214cb1cbdde14447b87fd2
ettom <36895504+ettom@users.noreply.github.com>
2019-10-30 21:02:57 +0200

    Add constructor for InvalidCityException

--- core/exceptions.h
+++ core/exceptions.h
@@ -1,5 +1,8 @@
+#pragma once
 #include <stdexcept>
 
-class InvalidCityException: public std::runtime_error
+class InvalidCityException : public std::runtime_error
 {
+public:
+	InvalidCityException(): std::runtime_error("Received city does not match the given city") {}
 };
