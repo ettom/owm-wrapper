c15271b475129d61cda51d31c1a1cf78522646c9
ettom <36895504+ettom@users.noreply.github.com>
2019-10-30 22:20:00 +0200

    Pass json as const reference for get_string_value

--- core/parser.cpp
+++ core/parser.cpp
@@ -10,7 +10,7 @@ std::string get_city(const std::string& response)
 	return to_json(response)["name"];
 }
 
-std::string get_string_value(json& input)
+std::string get_string_value(const json& input)
 {
 	if (input.is_string()) {
 		return input.get<std::string>();
