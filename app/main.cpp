9274cd9e1786ec06c30a1b12953803192eb9efac
ettom <36895504+ettom@users.noreply.github.com>
2019-11-25 20:37:51 +0200

    Prepare for proper options parsing

--- app/main.cpp
+++ app/main.cpp
@@ -2,26 +2,51 @@
 #include <sstream>
 #include <string>
 
+#include "CLI11.hpp"
+
 #include "core.h"
+#include "filesystem.h"
 #include "helpers.h"
+#include "temperature_unit.h"
 #include "weather_getter.h"
 #include "weather_reporter.h"
-#include "filesystem.h"
-#include "temperature_unit.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	if (argc < 2) {
-		std::cerr << "Pass a file with city names as an argument. Output will be written to output.json" << std::endl;
-		return EXIT_FAILURE;
+	CLI::App app { "Generate daily weather reports using the OpenWeatherMap API." };
+	std::string input_filename;
+	std::string output_filename;
+	TemperatureUnit::Unit unit = TemperatureUnit::CELSIUS;
+	app.add_option("-i,--input", input_filename,
+		       "Path to the input file containing the city names for "
+		       "which reports are to be generated")
+	    ->required()
+	    ->check(CLI::ExistingFile);
+
+	app.add_option("-o,--output", output_filename,
+		       "Where to write the weather reports. If no output path "
+		       "is provided, result will be written to stdout");
+
+	using namespace TemperatureUnit;
+	std::array<std::pair<Unit, const char*>, SIZE_OF_ENUM> arr = TemperatureUnit::make_unit_displayname_pairs();
+	std::vector<std::pair<Unit, const char*>> v(std::begin(arr), std::end(arr));
+
+	for (auto i : v) {
+		std::cout << i.first << std::endl;
+		std::cout << i.second << std::endl;
 	}
 
-	const std::vector<std::string> lines = read_file(argv[1]);
+	app.add_option("-u,--unit", unit, "Temperature unit")->transform(CLI::Transformer(v));
+
+	CLI11_PARSE(app, argc, argv);
+	std::cout << unit << std::endl;
+
+	const std::vector<std::string> lines = read_file(input_filename);
 
 	const WeatherGetter getter;
 	QueryParameters q;
 	q.timezone_offset = get_system_timezone_offset();
-	q.temperature_unit = TemperatureUnit::CELSIUS;
+	q.temperature_unit = unit;
 
 	json result = json::array();
 
@@ -36,6 +61,9 @@ int main(int argc, char *argv[])
 	}
 
 	std::cout << result.dump(4) << std::endl;
-	write_json_to_file("output.json", result);
+	if (!output_filename.empty()) {
+		write_json_to_file(output_filename, result);
+	}
+
 	return EXIT_SUCCESS;
 }
--- core/temperature_unit.h
+++ core/temperature_unit.h
@@ -2,16 +2,17 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <array>
 
 namespace TemperatureUnit
 {
 enum Unit { CELSIUS, FAHRENHEIT, KELVIN, SIZE_OF_ENUM };
-static const std::string for_display[] { "Celsius", "Fahrenheit", "Kelvin" };
-static const std::string for_OWM[] { "metric", "imperial", "default" };
+static const char* for_display[] { "Celsius", "Fahrenheit", "Kelvin" };
+static const char* for_OWM[] { "metric", "imperial", "default" };
 
-static_assert(sizeof(for_display) / sizeof(std::string) == SIZE_OF_ENUM
+static_assert(sizeof(for_display) / sizeof(char*) == SIZE_OF_ENUM
 	      , "Sizes don't match");
-static_assert(sizeof(for_OWM) / sizeof(std::string) == SIZE_OF_ENUM
+static_assert(sizeof(for_OWM) / sizeof(char*) == SIZE_OF_ENUM
 	      , "Sizes don't match");
 
 inline Unit match_display_name_to_unit(const std::string& display_name)
@@ -20,5 +21,15 @@ inline Unit match_display_name_to_unit(const std::string& display_name)
 						std::find(std::begin(for_display),
 								std::end(for_display),
 								display_name))));
+}
+
+constexpr std::array<std::pair<Unit, const char*>, SIZE_OF_ENUM> make_unit_displayname_pairs()
+{
+	std::array<std::pair<Unit, const char*>, SIZE_OF_ENUM> result;
+	for (size_t i = 0; i < SIZE_OF_ENUM; ++i) {
+		result[i] = std::make_pair(static_cast<Unit>(i), for_display[i]);
+	}
+
+	return result;
 }
 }
